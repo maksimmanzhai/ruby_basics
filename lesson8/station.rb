6e30cba318a3239de765ddb450c642cfb0d9374a
maksimmanzhai <bender23max@gmail.com>
2019-11-07 19:16:15 +0300

    Copy files from lesson7

---
+++ lesson8/station.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# class station
+class Station
+  include InstanceCounter
+
+  attr_reader :trains, :name, :stations
+
+  @@stations = []
+
+  def initialize(name)
+    @name = name
+    @trains = []
+    @@stations << self
+    validate!
+    self.register_instance
+  end
+
+  def validate!
+    raise "ERROR: Name of station can't be empty" if @name.empty?
+    raise "ERROR: Name of station should be at least 2 symbols" if @name.length < 2
+  end
+
+  def add_train(train)
+    @trains << train
+  end
+
+  def trains_by_type(type)
+    trains.select { |number, train| train.type == type }.size
+  end
+
+  def delete_train(train)
+    @trains.delete(train.number)
+  end
+
+  def each_train
+    @trains.each do |train|
+      yield(train) if block_given?
+    end
+  end
+
+  class << self
+    def all
+      @@stations
+    end
+  end
+end
---
+++ lesson8/test.rb
@@ -0,0 +1,54 @@
+station1 = Station.new('minsk')
+station2 = Station.new('hrodna')
+station3 = Station.new('vitebsk')
+station4 = Station.new('mohilev')
+station5 = Station.new('homel')
+station6 = Station.new('brest')
+
+train1 = CargoTrain.new('123 33', 'Flagstaff')
+train2 = CargoTrain.new('452 14', 'Accessories')
+train3 = CargoTrain.new('223 82', 'Iconic')
+train4 = PassengerTrain.new('876 26', 'Pronto')
+train5 = PassengerTrain.new('342 54', 'Simplified')
+
+route1 = Route.new('minsk_mohilev', station1, station4)
+route2 = Route.new('hrodna_brest', station2, station6)
+route3 = Route.new('vitebsk_hrodna', station3, station2)
+route4 = Route.new('mohilev_vitebsk', station4, station3)
+route5 = Route.new('homel_minsk', station5, station1)
+route6 = Route.new('brest_homel', station6, station5)
+
+route1.add_station(station3)
+route1.add_station(station4)
+route2.add_station(station5)
+route3.add_station(station1)
+
+route1.delete_station(station4)
+
+train1.set_route(route1)
+train2.set_route(route2)
+train3.set_route(route3)
+train4.set_route(route4)
+train5.set_route(route5)
+
+carriage1 = CargoCarriage.new(123, 'Lexx')
+carriage2 = PassengerCarriage.new(40, 'Ritine')
+carriage3 = CargoCarriage.new(200, 'Asebx')
+carriage4 = CargoCarriage.new(213, 'Asebx')
+carriage5 = CargoCarriage.new(190, 'Asebx')
+carriage6 = CargoCarriage.new(200, 'Lexx')
+carriage7 = CargoCarriage.new(110, 'Lexx')
+
+train1.coupling_carriages(carriage1)
+train1.coupling_carriages(carriage2)
+train1.coupling_carriages(carriage6)
+train1.coupling_carriages(carriage7)
+
+train2.coupling_carriages(carriage3)
+train2.coupling_carriages(carriage4)
+train2.coupling_carriages(carriage5)
+
+train2.uncoupling_carriages
+train2.uncoupling_carriages
+train1.move_forward
+train1.move_back
